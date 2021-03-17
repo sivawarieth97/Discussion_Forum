886807c7d0eff531f23ef8e6faa2191b505a10b1
sivawarieth97 <sivawarieth97@gmail.com>
2021-03-17 17:46:56 +0530

    initial commit

---
+++ passport.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+const dbUrl="mongodb://localhost/app";
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user
+Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+  console.log('mongodb connected',err);
+}) 
+
+var server = http.listen(4000, () => {
+  console.log('server is running on port', server.address().port);
+});
